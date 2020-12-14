18de95a861c8bd969d7726e1566276a4e64ff012
Vikas Singh <vikas3091999@gmail.com>
2020-12-14 20:52:56 +0530

    Added BLE to webapp communication

--- stackswan-gateway.ino
+++ stackswan-gateway.ino
@@ -17,6 +17,57 @@
 
 #include <SPI.h>              
 #include <LoRa.h>             
+#include <ArduinoJson.h> //ArduinoJson V6
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+std::string deviceName = "Stackswan Gateway";
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+BLECharacteristic* pCharacteristicR = NULL;
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+#define SERVICE_UUID        "afa2f203-348f-4718-9cf3-f7ff0de38472"
+#define CHARACTERISTIC_UUID "450fcc81-dad5-46a6-959f-c5f41e37b669"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      BLEDevice::startAdvertising();
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+
+#define CHARACTERISTIC_UUID_RX "10fd2313-62c2-4053-b3af-7e6ca407bf21"
+class MyCallbacks: public BLECharacteristicCallbacks {
+
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+      Serial.println(rxValue[0]);
+
+      if (rxValue.length() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+
+        for (int i = 0; i < rxValue.length(); i++) {
+          Serial.print(rxValue[i]);
+        }
+        Serial.println();
+        Serial.println("*********");
+      }
+
+    }
+};
 
 const int csPin = 5;          // LoRa radio chip select
 const int resetPin = 14;      // LoRa radio reset
@@ -33,7 +84,8 @@ int interval = 2000;          // initial interval between sends
 void setup() {
   
   // initialize serial
-  Serial.begin(115200);                   
+  Serial.begin(115200); 
+  setupBlueTooth();                  
   while (!Serial);
   Serial.println("stackswan P2P");
   
@@ -107,4 +159,76 @@ void onReceive(int packetSize) {
   Serial.println("RSSI: " + String(LoRa.packetRssi()));
   Serial.println("Snr: " + String(LoRa.packetSnr()));
   Serial.println();
+
+  // JSON modeling the raw data 
+  const int bufferSize = 4 *  JSON_OBJECT_SIZE(4);
+  StaticJsonDocument<bufferSize> doc;
+
+  doc["SenderID"]  = sender;
+  doc["ReceiverID"] = recipient;
+  doc["Payload"] = incoming;
+
+  String jsonstat;
+  Serial.println(jsonstat);
+  serializeJson(doc, jsonstat);
+
+  // Pretty print on Serial
+  serializeJsonPretty(doc, Serial);
+
+
+  size_t dataLen = jsonstat.length();
+  pCharacteristic->setValue((uint8_t*)&jsonstat[0], dataLen);
+  pCharacteristic->notify();
+  
 }
+
+/**
+   setupBlueTooth
+*/
+void setupBlueTooth()
+{
+  // Create the BLE Device
+  BLEDevice::init(deviceName);
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  /*
+    pCharacteristicR = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID_RX,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+    pCharacteristicR->setCallbacks(new MyCallbacks());
+  */
+ 
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
